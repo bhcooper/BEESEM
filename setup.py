746ddb98338144e3a45be610c519673701cf8eff
Shuxiang Ruan <sruan@wustl.edu>
2017-11-04 00:37:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 
 setup(
     name='BEESEM',
-    version='1.0.0',
+    version='1.0.1',
     packages=['toolbox'],
-    url='http://stormo.wustl.edu/resources.html',
+    url='http://genetics.wustl.edu/gslab/resources/',
     license='GNU GPLv3',
     author='Shuxiang Ruan',
     author_email='sruan@wustl.edu',
